f7fac098eb609cab7b062ea2b429f9bc00b3a713
Stefan Woods <stefan5365@hotmail.com>
2020-10-05 14:21:38 -0700

    First commit of AddingMachine

---
+++ AddingMachine/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,38 @@
+/**
+ * Name: Stefan Woods
+ * Date: 10/5/20
+ * Description: Class AddingMachine can add, subtract and return a total 
+ */
+
+package cse360assignment02;
+
+public class AddingMachine {
+	  /**
+	   * variables
+	   */
+	  private int total;
+	  
+	  /**
+	   * constructor
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  public void add (int value) {
+	  }
+
+	  public void subtract (int value) {
+	  }
+
+	  public String toString () {
+	    return "";
+	  }
+
+	  public void clear() {
+	  }
+	}
